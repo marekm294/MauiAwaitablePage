928fb02b27fd1f80e716e1301f72a7102582d4a1
Marek Mička <marek.micka@conware.eu>
2023-05-03 17:01:23 +0200

    Add project files.

---
+++ MauiAwaitablePage/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiAwaitablePage
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ MauiAwaitablePage/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiAwaitablePage
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
